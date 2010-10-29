01323c65d947476a2765c9529b2e98f9e6ebc073
orbitz <orbitz@gmail.com>
2010-10-28 21:20:36 -0400

    Adding some useful functions to Ort.Function

--- lib/ort_function.ml
+++ lib/ort_function.ml
@@ -2,3 +2,8 @@
 let (|>) d f = f d
 
 let flip f x y = f y x
+
+let const x _ = x
+
+let identity x = x
+
--- lib/ort_function.mli
+++ lib/ort_function.mli
@@ -1,2 +1,4 @@
 val ( |> ) : 'a -> ('a -> 'b) -> 'b
 val flip : ('a -> 'b -> 'c) -> 'b -> 'a -> 'c
+val const : 'a -> 'b -> 'a
+val identity : 'a -> 'a
